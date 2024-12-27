f149369614ec0d05ee015966c89cf0fcb8b65541
Gabriel Radzki <gabriel.radzki@gmail.com>
2024-12-27 18:18:10 -0300

    getAmigoSecreto fn

--- xss.js
+++ xss.js
@@ -1,5 +1,6 @@
 function tiago() {
   alert("Trollados by rdz");
+  alert("Você tirou: " + getAmigoSecreto())
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -9,3 +10,11 @@ document.addEventListener('DOMContentLoaded', function () {
 document.body.addEventListener('click', function () {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 });
+
+function removeNonAlphanumeric(inputString) {
+    return inputString.replace(/[^A-Z\d\s]/gi, '');
+};
+
+function getAmigoSecreto() {
+   return $x('/html/body/div[2]/div[6]/div/div/div/div/div[2]/div[1]/form[3]/div/div/p/script/text()')[0].wholeText.split('amigoTireiAberto')[1].split('strong')[1]
+}
