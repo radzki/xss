3b93e5518c2aa966a89e3310af63320329f0e2bf
Gabriel Radzki <gabriel.radzki@gmail.com>
2024-12-27 18:19:21 -0300

    getAmigoSecreto fn 2

--- xss.js
+++ xss.js
@@ -1,3 +1,11 @@
+function removeNonAlphanumeric(inputString) {
+    return inputString.replace(/[^A-Z\d\s]/gi, '');
+};
+
+function getAmigoSecreto() {
+   return $x('/html/body/div[2]/div[6]/div/div/div/div/div[2]/div[1]/form[3]/div/div/p/script/text()')[0].wholeText.split('amigoTireiAberto')[1].split('strong')[1]
+};
+
 function tiago() {
   alert("Trollados by rdz");
   alert("Você tirou: " + getAmigoSecreto())
@@ -11,10 +19,3 @@ document.body.addEventListener('click', function () {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 });
 
-function removeNonAlphanumeric(inputString) {
-    return inputString.replace(/[^A-Z\d\s]/gi, '');
-};
-
-function getAmigoSecreto() {
-   return $x('/html/body/div[2]/div[6]/div/div/div/div/div[2]/div[1]/form[3]/div/div/p/script/text()')[0].wholeText.split('amigoTireiAberto')[1].split('strong')[1]
-}
